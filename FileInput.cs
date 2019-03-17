7c10870fe95479ff4ec6a7416cada3b747aa444b
AidilRiski <riskisultan@yahoo.com>
2019-03-17 21:22:54 +0700

    FileInput error is fixed.

--- FileInput.cs
+++ FileInput.cs
@@ -7,7 +7,7 @@ public class FileReader
     public int[][] inputArray;
     public int nVertex;
 
-        static void Main()
+    static void Main()
     {
         // base stack level
         int x = 0;  
@@ -25,7 +25,7 @@ public class FileReader
         }
     }
 
-        public static void ParseNumberFile(string filename, int[][] inputArray, int nVertex)
+    public static void ParseNumberFile(string filename, int[][] inputArray, int nVertex)
     {
         string fileContent = File.ReadAllText(filename);
         string[] integerStrings = fileContent.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
@@ -35,8 +35,8 @@ public class FileReader
         int[][] _inputArray = new int[integerStrings.Length-1][];
 
         for (int n = 1; n < integerStrings.Length-1; n++){
-                _inputArray[n][0] = int.Parse(integerStrings[n]);
-                _inputArray[n][1] = int.Parse(integerStrings[n]);
+            _inputArray[n] = new int[2]{int.Parse(integerStrings[n]), int.Parse(integerStrings[n])};
+            Console.Out.WriteLine(_inputArray[n][0].ToString() + _inputArray[n][1].ToString());
         }  
 
         inputArray = _inputArray; //gapapa bitwise karena sama
