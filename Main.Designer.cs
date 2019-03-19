7040d32bf52fd22afb1a385456cdf82d7ce7f84a
AidilRiski <riskisultan@yahoo.com>
2019-03-19 22:01:57 +0700

    Change title and icon.

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.titleLabel = new System.Windows.Forms.Label();
             this.solveButton = new System.Windows.Forms.Button();
             this.pathBox = new System.Windows.Forms.TextBox();
@@ -51,9 +52,10 @@
             this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.titleLabel.AutoSize = true;
             this.titleLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(16, 21);
+            this.titleLabel.Location = new System.Drawing.Point(11, 14);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(235, 37);
+            this.titleLabel.Size = new System.Drawing.Size(159, 26);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "Hide and Seek";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -62,30 +64,29 @@
             // 
             this.solveButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.solveButton.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.solveButton.Location = new System.Drawing.Point(20, 409);
-            this.solveButton.MinimumSize = new System.Drawing.Size(82, 42);
+            this.solveButton.Location = new System.Drawing.Point(13, 266);
+            this.solveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.solveButton.MinimumSize = new System.Drawing.Size(55, 27);
             this.solveButton.Name = "solveButton";
-            this.solveButton.Size = new System.Drawing.Size(82, 42);
+            this.solveButton.Size = new System.Drawing.Size(55, 27);
             this.solveButton.TabIndex = 1;
             this.solveButton.Text = "Cari!";
             this.solveButton.UseVisualStyleBackColor = true;
             // 
             // pathBox
             // 
-            this.pathBox.Location = new System.Drawing.Point(160, 12);
-            this.pathBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pathBox.Location = new System.Drawing.Point(107, 8);
             this.pathBox.Name = "pathBox";
-            this.pathBox.Size = new System.Drawing.Size(268, 26);
+            this.pathBox.Size = new System.Drawing.Size(180, 20);
             this.pathBox.TabIndex = 2;
             // 
             // pathLabel
             // 
             this.pathLabel.AutoSize = true;
             this.pathLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pathLabel.Location = new System.Drawing.Point(10, 9);
-            this.pathLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.pathLabel.Location = new System.Drawing.Point(7, 6);
             this.pathLabel.Name = "pathLabel";
-            this.pathLabel.Size = new System.Drawing.Size(69, 27);
+            this.pathLabel.Size = new System.Drawing.Size(44, 18);
             this.pathLabel.TabIndex = 3;
             this.pathLabel.Text = "Path:";
             // 
@@ -98,36 +99,32 @@
             this.inputForm.Controls.Add(this.queryPathLabel);
             this.inputForm.Controls.Add(this.pathLabel);
             this.inputForm.Controls.Add(this.pathBox);
-            this.inputForm.Location = new System.Drawing.Point(19, 63);
-            this.inputForm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.inputForm.Location = new System.Drawing.Point(13, 41);
             this.inputForm.Name = "inputForm";
-            this.inputForm.Size = new System.Drawing.Size(521, 114);
+            this.inputForm.Size = new System.Drawing.Size(347, 74);
             this.inputForm.TabIndex = 4;
             // 
             // queryPathBox
             // 
-            this.queryPathBox.Location = new System.Drawing.Point(160, 72);
-            this.queryPathBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.queryPathBox.Location = new System.Drawing.Point(107, 47);
             this.queryPathBox.Name = "queryPathBox";
-            this.queryPathBox.Size = new System.Drawing.Size(268, 26);
+            this.queryPathBox.Size = new System.Drawing.Size(180, 20);
             this.queryPathBox.TabIndex = 7;
             // 
             // graphBrowseButton
             // 
-            this.graphBrowseButton.Location = new System.Drawing.Point(439, 9);
-            this.graphBrowseButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.graphBrowseButton.Location = new System.Drawing.Point(293, 6);
             this.graphBrowseButton.Name = "graphBrowseButton";
-            this.graphBrowseButton.Size = new System.Drawing.Size(56, 35);
+            this.graphBrowseButton.Size = new System.Drawing.Size(37, 23);
             this.graphBrowseButton.TabIndex = 4;
             this.graphBrowseButton.Text = "...";
             this.graphBrowseButton.UseVisualStyleBackColor = true;
             // 
             // queryBrowseButton
             // 
-            this.queryBrowseButton.Location = new System.Drawing.Point(439, 67);
-            this.queryBrowseButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.queryBrowseButton.Location = new System.Drawing.Point(293, 44);
             this.queryBrowseButton.Name = "queryBrowseButton";
-            this.queryBrowseButton.Size = new System.Drawing.Size(56, 35);
+            this.queryBrowseButton.Size = new System.Drawing.Size(37, 23);
             this.queryBrowseButton.TabIndex = 6;
             this.queryBrowseButton.Text = "...";
             this.queryBrowseButton.UseVisualStyleBackColor = true;
@@ -136,19 +133,17 @@
             // 
             this.queryPathLabel.AutoSize = true;
             this.queryPathLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.queryPathLabel.Location = new System.Drawing.Point(10, 72);
-            this.queryPathLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.queryPathLabel.Location = new System.Drawing.Point(7, 47);
             this.queryPathLabel.Name = "queryPathLabel";
-            this.queryPathLabel.Size = new System.Drawing.Size(142, 27);
+            this.queryPathLabel.Size = new System.Drawing.Size(89, 18);
             this.queryPathLabel.TabIndex = 5;
             this.queryPathLabel.Text = "Query Path:";
             // 
             // queryBox
             // 
-            this.queryBox.Location = new System.Drawing.Point(17, 54);
-            this.queryBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.queryBox.Location = new System.Drawing.Point(11, 35);
             this.queryBox.Name = "queryBox";
-            this.queryBox.Size = new System.Drawing.Size(180, 26);
+            this.queryBox.Size = new System.Drawing.Size(121, 20);
             this.queryBox.TabIndex = 5;
             // 
             // hasilLabel
@@ -156,10 +151,9 @@
             this.hasilLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.hasilLabel.AutoSize = true;
             this.hasilLabel.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.hasilLabel.Location = new System.Drawing.Point(16, 297);
-            this.hasilLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.hasilLabel.Location = new System.Drawing.Point(11, 193);
             this.hasilLabel.Name = "hasilLabel";
-            this.hasilLabel.Size = new System.Drawing.Size(105, 37);
+            this.hasilLabel.Size = new System.Drawing.Size(65, 24);
             this.hasilLabel.TabIndex = 5;
             this.hasilLabel.Text = "Hasil:";
             // 
@@ -167,10 +161,11 @@
             // 
             this.nextQueryButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.nextQueryButton.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nextQueryButton.Location = new System.Drawing.Point(20, 352);
-            this.nextQueryButton.MinimumSize = new System.Drawing.Size(82, 42);
+            this.nextQueryButton.Location = new System.Drawing.Point(13, 229);
+            this.nextQueryButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nextQueryButton.MinimumSize = new System.Drawing.Size(55, 27);
             this.nextQueryButton.Name = "nextQueryButton";
-            this.nextQueryButton.Size = new System.Drawing.Size(177, 42);
+            this.nextQueryButton.Size = new System.Drawing.Size(118, 27);
             this.nextQueryButton.TabIndex = 6;
             this.nextQueryButton.Text = "Next Query";
             this.nextQueryButton.UseVisualStyleBackColor = true;
@@ -179,9 +174,10 @@
             // 
             this.queryLabel.AutoSize = true;
             this.queryLabel.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.queryLabel.Location = new System.Drawing.Point(11, 13);
+            this.queryLabel.Location = new System.Drawing.Point(7, 8);
+            this.queryLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.queryLabel.Name = "queryLabel";
-            this.queryLabel.Size = new System.Drawing.Size(96, 33);
+            this.queryLabel.Size = new System.Drawing.Size(67, 22);
             this.queryLabel.TabIndex = 7;
             this.queryLabel.Text = "Query";
             // 
@@ -190,25 +186,28 @@
             this.queryPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.queryPanel.Controls.Add(this.queryBox);
             this.queryPanel.Controls.Add(this.queryLabel);
-            this.queryPanel.Location = new System.Drawing.Point(19, 190);
+            this.queryPanel.Location = new System.Drawing.Point(13, 123);
+            this.queryPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.queryPanel.Name = "queryPanel";
-            this.queryPanel.Size = new System.Drawing.Size(213, 100);
+            this.queryPanel.Size = new System.Drawing.Size(142, 65);
             this.queryPanel.TabIndex = 8;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(578, 464);
+            this.ClientSize = new System.Drawing.Size(385, 302);
             this.Controls.Add(this.queryPanel);
             this.Controls.Add(this.nextQueryButton);
             this.Controls.Add(this.hasilLabel);
             this.Controls.Add(this.inputForm);
             this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.solveButton);
-            this.MinimumSize = new System.Drawing.Size(499, 339);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MinimumSize = new System.Drawing.Size(338, 234);
             this.Name = "Main";
-            this.Text = "Main";
+            this.Text = "Hide and Seek";
             this.inputForm.ResumeLayout(false);
             this.inputForm.PerformLayout();
             this.queryPanel.ResumeLayout(false);
